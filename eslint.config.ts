ff28e0e37320465dad93e61ab6d1e85dd7d3511c
Neko Ayaka <neko@ayaka.moe>
2025-06-11 15:26:30 +0800

    chore(deps): bump dependencies

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,15 @@
 import antfu from '@antfu/eslint-config'
+import { importX } from 'eslint-plugin-import-x'
 
 export default await antfu(
   {
     unocss: true,
     vue: true,
     rules: {
-      'import/order': [
+      'ts/ban-ts-comment': 'off',
+      'antfu/import-dedupe': 'error',
+      'style/padding-line-between-statements': 'error',
+      'import-x/order': [
         'error',
         {
           'groups': [
@@ -16,6 +20,9 @@ export default await antfu(
           'newlines-between': 'always',
         },
       ],
+    },
+    plugins: {
+      'import-x': importX,
     },
   },
 )
---
+++ packages/eval/tsdown.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsdown'
+
+export default defineConfig({
+  entry: {
+    index: './src/index.ts',
+    node: './src/node.ts',
+    browser: './src/browser.ts',
+  },
+  sourcemap: true,
+  unused: true,
+  fixedExtension: true,
+  attw: true,
+})
